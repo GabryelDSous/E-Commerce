4217202ea730708ea31f429e33db608f1a214763
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-04-30 20:43:26 -0300

    feat(user-service): add user login method to authenticate and generate token

--- src/main/java/dev/gabryel/ecommerce/service/UserService.java
+++ src/main/java/dev/gabryel/ecommerce/service/UserService.java
@@ -1,11 +1,15 @@
 package dev.gabryel.ecommerce.service;
 
+import dev.gabryel.ecommerce.config.TokenConfig;
+import dev.gabryel.ecommerce.dto.user.request.UserLoginRequest;
 import dev.gabryel.ecommerce.dto.user.request.UserRegisterRequest;
 import dev.gabryel.ecommerce.mapper.UserMapper;
 import dev.gabryel.ecommerce.model.UserModel;
 import dev.gabryel.ecommerce.model.enums.UserRoles;
 import dev.gabryel.ecommerce.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +17,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenConfig tokenConfig;
+    private final AuthenticationManager authenticationManager;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenConfig tokenConfig, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.tokenConfig = tokenConfig;
+        this.authenticationManager = authenticationManager;
     }
 
     @Transactional
@@ -29,5 +37,13 @@ public class UserService {
         }
         userRepository.save(userModel);
         return userModel.getEmail();
+    }
+
+    public String userLogin(UserLoginRequest userRequest) {
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(
+                userRequest.email(), userRequest.password()
+        );
+        UserModel userModel = (UserModel) authenticationManager.authenticate(userAndPass).getPrincipal();
+        return tokenConfig.generateToken(userModel);
     }
 }
