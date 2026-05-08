6e001a0346b218772d1c7ef701d41916be7092bc
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-05-08 16:22:33 -0300

    feat(product-service): add list all product method

--- src/main/java/dev/gabryel/ecommerce/service/ProductService.java
+++ src/main/java/dev/gabryel/ecommerce/service/ProductService.java
@@ -2,7 +2,9 @@ package dev.gabryel.ecommerce.service;
 
 import dev.gabryel.ecommerce.config.JWTUserData;
 import dev.gabryel.ecommerce.dto.product.request.ProductRegisterRequest;
+import dev.gabryel.ecommerce.dto.product.response.ProductListResponse;
 import dev.gabryel.ecommerce.dto.product.response.ProductRegisterResponse;
+import dev.gabryel.ecommerce.exception.ProductException;
 import dev.gabryel.ecommerce.exception.UserException;
 import dev.gabryel.ecommerce.mapper.ProductMapper;
 import dev.gabryel.ecommerce.model.ProductModel;
@@ -12,6 +14,8 @@ import dev.gabryel.ecommerce.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -30,5 +34,14 @@ public class ProductService {
         productModel.setSellerName(userModel.getName());
         productRepository.save(productModel);
         return ProductMapper.toProductRegisterResponse(productModel);
+    }
+
+    public List<ProductListResponse> productListAll() {
+        List<ProductModel> productModels = productRepository.findAll();
+        if (productModels.isEmpty())
+            throw new ProductException("Products not found", HttpStatus.NOT_FOUND.value());
+        return productModels.stream()
+                .map(ProductMapper::toProductListResponse)
+                .toList();
     }
 }
