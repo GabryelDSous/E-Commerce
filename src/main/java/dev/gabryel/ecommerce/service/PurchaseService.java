1282459e48693ad7337396b0797b8dc5dac5254f
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-05-15 23:54:06 -0300

    feat(purchase-service): add method to service for buying product with USER role authorization

---
+++ src/main/java/dev/gabryel/ecommerce/service/PurchaseService.java
@@ -0,0 +1,50 @@
+package dev.gabryel.ecommerce.service;
+
+import dev.gabryel.ecommerce.config.JWTUserData;
+import dev.gabryel.ecommerce.dto.purchase.request.PurchaseProductRequest;
+import dev.gabryel.ecommerce.dto.purchase.response.PurchaseProductResponse;
+import dev.gabryel.ecommerce.exception.ProductException;
+import dev.gabryel.ecommerce.exception.UserException;
+import dev.gabryel.ecommerce.mapper.PurchaseMapper;
+import dev.gabryel.ecommerce.model.ProductModel;
+import dev.gabryel.ecommerce.model.PurchaseModel;
+import dev.gabryel.ecommerce.model.UserModel;
+import dev.gabryel.ecommerce.repository.ProductRepository;
+import dev.gabryel.ecommerce.repository.PurchaseRepository;
+import dev.gabryel.ecommerce.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.postgresql.util.PSQLException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PurchaseService {
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseService(UserRepository userRepository, ProductRepository productRepository, PurchaseRepository purchaseRepository) {
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @Transactional
+    public PurchaseProductResponse purchaseProduct(JWTUserData userData, PurchaseProductRequest purchaseRequest) {
+        UserModel userModel = userRepository.findById(UUID.fromString(userData.userId()))
+                .orElseThrow(() -> new UserException("Users not found", HttpStatus.NOT_FOUND.value()));
+        ProductModel productModel = productRepository.findById(purchaseRequest.id())
+                .orElseThrow(() -> new ProductException("Product not found", HttpStatus.NOT_FOUND.value()));
+        int decreased = productRepository.decreaseStock(productModel.getName(), purchaseRequest.quantity());
+        if (decreased == 0)
+            throw new RuntimeException("Product cannot be purchased");
+        PurchaseModel purchaseModel = PurchaseMapper.toPurchaseModel(userModel, productModel);
+        purchaseModel.setQuantity(purchaseRequest.quantity());
+        purchaseRepository.save(purchaseModel);
+        return PurchaseMapper.toPurchaseProductResponse(purchaseModel);
+    }
+}
